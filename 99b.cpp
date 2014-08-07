a8aff7c59a4e8c2231f12ee97aab1fc8a3007e4e
jneg <jneg@users.noreply.github.com>
2014-08-07 13:15:18 -0700

    Saved 4 characters on 99 bottles

--- 99b.cpp
+++ 99b.cpp
@@ -7,7 +7,7 @@
 
 std::string b(int i)
 {
-   if(i==0) return "no more bottles";
+   if(!i) return "no more bottles";
    return std::to_string(i) + (i==1 ? " bottle" : " bottles");
 }
 
@@ -16,7 +16,7 @@ int main()
    for(int i=99; i>=0; --i)
    {
       std::cout << b(i) << " of beer on the wall, " << b(i) << " of beer.\n";
-      if(i==0) std::cout << "Go to the store and buy some more, 99 bottles of beer on the wall.\n\n";
+      if(!i) std::cout << "Go to the store and buy some more, 99 bottles of beer on the wall.\n\n";
       else std::cout << "Take one down and pass it around, " << b(i-1) << " of beer on the wall.\n\n";
    }
 }
