6a78a77b5aafb7379e4d7994e3d9dd3b2660f0d7
jneg <jneg@users.noreply.github.com>
2014-08-06 21:05:55 -0700

    Optimized 99 Bottles program with ternary operator

--- 99b.cpp
+++ 99b.cpp
@@ -4,18 +4,15 @@
 
 #include <cstdint>
 #include <iostream>
+#include <string>
 
 int main()
 {
-   for(std::int16_t b=99; b!=2; --b)
+   for(std::int16_t b=99; b!=0; --b)
    {
-      std::cout << b << " bottles of beer on the wall, " << b << " bottles of beer." << std::endl;
-      std::cout << "Take one down and pass it around, " << b-1 << " bottles of beer on the wall.\n" << std::endl;
+      std::cout << b << (b==1 ? " bottle" : " bottles") << " of beer on the wall, " << b << (b==1 ? " bottle" : " bottles") << " of beer." << std::endl;
+      std::cout << "Take one down and pass it around, " << (b-1==0 ? "no more" : std::to_string(b-1)) << (b-1==1 ? " bottle" : " bottles") << " of beer on the wall.\n" << std::endl;
    }
-   std::cout << "2 bottles of beer on the wall, 2 bottles of beer." << std::endl;
-   std::cout << "Take one down and pass it around, 1 bottle of beer on the wall.\n" << std::endl;
-   std::cout << "1 bottle of beer on the wall, 1 bottle of beer." << std::endl;
-   std::cout << "Take one down and pass it around, no more  bottles of beer on the wall.\n" << std::endl;
    std::cout << "No more bottles of beer on the wall, no more bottles of beer." << std::endl;
    std::cout << "Go to the store and buy some more, 99 bottles of beer on the wall.\n" << std::endl;
 }
