43b6d00d1816affc928f3e3259dba897bc64de97
jneg <jneg@users.noreply.github.com>
2014-08-07 12:51:53 -0700

    Optimized 99 Bottles to 502C

--- 99b.cpp
+++ 99b.cpp
@@ -2,23 +2,21 @@
    99 Bottles of Beer on the Wall Program
 */
 
-#include <cstdint>
 #include <iostream>
 #include <string>
 
-std::string b(std::int16_t i)
+std::string b(int i)
 {
    if(i==0) return "no more bottles";
-   else return std::to_string(i) + (i==1 ? " bottle" : " bottles");
+   return std::to_string(i) + (i==1 ? " bottle" : " bottles");
 }
 
 int main()
 {
-   for(std::int16_t i=99; i!=0; --i)
+   for(int i=99; i>=0; --i)
    {
-      std::cout << b(i) << " of beer on the wall, " << b(i) << " of beer." << std::endl;
-      std::cout << "Take one down and pass it around, " << b(i-1) << " of beer on the wall.\n" << std::endl;
+      std::cout << b(i) << " of beer on the wall, " << b(i) << " of beer.\n";
+      if(i==0) std::cout << "Go to the store and buy some more, 99 bottles of beer on the wall.\n\n";
+      else std::cout << "Take one down and pass it around, " << b(i-1) << " of beer on the wall.\n\n";
    }
-   std::cout << "No more bottles of beer on the wall, no more bottles of beer." << std::endl;
-   std::cout << "Go to the store and buy some more, 99 bottles of beer on the wall.\n" << std::endl;
 }
